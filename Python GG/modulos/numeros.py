2edc9d71c4bec16121f3457f0e36406017dd3fa8
uosp3 <edson_g_santos@yahoo.com.br>
2024-07-22 15:46:44 -0300

    ignore
    ignore

--- Python GG/modulos/numeros.py
+++
@@ -1,6 +0,0 @@
-import uteis.fatorial
-#from pacotes import uteis
-    
-num=int(input('Digite um valor: '))
-fat=uteis.fatorial.fatorial(num)
-print(f'O fatorial de {num} e {fat}.')
--- Python GG/modulos/uteis/fatorial.py
+++
@@ -1,5 +0,0 @@
-def fatorial(n):
-    f=1
-    for c in range(1, n+1):
-        f*=c
-    return f
