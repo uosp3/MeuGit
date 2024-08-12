3279270e756803cc26a3120c01092d437ea454ca
Edson Gomes dos Santos <110746401+uosp3@users.noreply.github.com>
2024-08-11 22:50:41 -0300

    Update settings.py
    secret key

--- Pythonando/novo_projeto/core/settings.py
+++ Pythonando/novo_projeto/core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vi8tv!5b!i5s__u3epw5zyn++$tp%@(pbz^-+j30yx^0f6wo%z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
